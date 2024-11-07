db83ac9f67755a9b45684f0f027fbd8475a6f9c6
Thaika <dummtmail@g.com>
2024-11-07 12:25:34 +0530

    add: express mongoose dotenv packages added

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Marhaba")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(" From Express js Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
